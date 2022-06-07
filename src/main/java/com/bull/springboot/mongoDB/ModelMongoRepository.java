91701f96724b8687b00e5440cd343a292f284690
Javier Eduardo Labat <javier.labat@sdc.com.ar>
2022-06-07 12:53:34 -0300

    Build Fixes

--- src/main/java/com/bull/springboot/mongoDB/ModelMongoRepository.java
+++ src/main/java/com/bull/springboot/mongoDB/ModelMongoRepository.java
@@ -1,8 +1,8 @@
 package com.bull.springboot.mongoDB;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+//import org.springframework.data.repository.PagingAndSortingRepository;
 
-import com.bull.springboot.application.model.Transcation;
+//import com.bull.springboot.application.model.Transcation;
 
-public interface  ModelMongoRepository extends  PagingAndSortingRepository<Transcation, String> {
+public interface  ModelMongoRepository{// extends  PagingAndSortingRepository<Transcation, String> {
 }     
