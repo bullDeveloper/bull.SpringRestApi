b393cf30626be3ccafa98a515bd21ec2290c6c05
Bull Coding <33255456+bullDeveloper@users.noreply.github.com>
2022-06-07 13:18:21 -0300

    Delete src/main directory

--- src/main/java/com/bull/springboot/mongoDB/ModelMongoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bull.springboot.mongoDB;
-
-//import org.springframework.data.repository.PagingAndSortingRepository;
-
-//import com.bull.springboot.application.model.Transcation;
-
-public interface  ModelMongoRepository{// extends  PagingAndSortingRepository<Transcation, String> {
-}     
