22f235f4cc6bff5bd2a3d4c95fdb18c5a639cb26
Bull Coding <33255456+bullDeveloper@users.noreply.github.com>
2022-06-07 15:51:21 -0300

    Delete src directory

--- src/main/java/com/example/democrud/dao/api/PersonaDaoAPI.java
+++
@@ -1,9 +0,0 @@
-package com.example.democrud.dao.api;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.democrud.model.Persona;
-
-public interface PersonaDaoAPI extends CrudRepository<Persona, Long> {
-
-}
--- src/main/java/com/example/democrud/service/api/PersonaServiceAPI.java
+++
@@ -1,8 +0,0 @@
-package com.example.democrud.service.api;
-
-import com.example.democrud.commons.GenericServiceAPI;
-import com.example.democrud.model.Persona;
-
-public interface PersonaServiceAPI extends GenericServiceAPI<Persona, Long>  {
-	
-}
